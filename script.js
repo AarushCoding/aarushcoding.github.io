a92120a5cbbba269ac4207c4c4ded30d011af714
Aarush Naik <109808496+AarushCoding@users.noreply.github.com>
2026-02-12 19:38:07 +0000

    fix issues

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.querySelectorAll('.nav-links a').forEach(link => {
-    if (link.href === window.location.href) {
-        link.style.color = "#007bff";
-        link.style.borderBottom = "2px solid #007bff";
-    }
-});
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const wheel = document.getElementById('title-wheel');
